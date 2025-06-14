885973441c60f583435000ddfc4117d389f31c23
Raihan Adam <hallwack.id@gmail.com>
2025-06-14 12:22:10 +0700

    fix: add input snacks (not working)

--- lua/hallwack/plugins/snacks.lua
+++ lua/hallwack/plugins/snacks.lua
@@ -1,23 +1,33 @@
 return {
   "folke/snacks.nvim",
-  priority = 1000,
+  priority = 100000,
   lazy = false,
   ---@type snacks.Config
   opts = {
-    bigfile = { enabled = false },
+    input = {
+      enabled = true,
+      icon = " ",
+      icon_hl = "SnacksInputIcon",
+      icon_pos = "left",
+      prompt_pos = "title",
+      win = { style = "input" },
+      expand = true,
+    },
     dashboard = { enabled = true },
     explorer = { enabled = true },
-    indent = { enabled = false },
-    input = { enabled = true },
-    picker = { enabled = false },
     notifier = { enabled = true },
-    quickfile = { enabled = false },
-    scope = { enabled = false },
     scroll = { enabled = true },
-    statuscolumn = { enabled = false },
-    words = { enabled = false },
+    git = { enabled = true },
   },
   keys = {
-    { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+    { "<leader>e",  function() Snacks.explorer() end,            desc = "File Explorer" },
+    { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+    { "<leader>gl", function() Snacks.picker.git_log() end,      desc = "Git Log" },
+    { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+    { "<leader>gs", function() Snacks.picker.git_status() end,   desc = "Git Status" },
+    { "<leader>gS", function() Snacks.picker.git_stash() end,    desc = "Git Stash" },
+    { "<leader>gd", function() Snacks.picker.git_diff() end,     desc = "Git Diff (Hunks)" },
+    { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+    { "<leader>ga", function() Snacks.git.blame_line() end,      desc = "Git Blame Line" },
   }
 }
